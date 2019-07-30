9dab6326fd463442f84915f938fbaa6ed7f0b400
lilak <kcabrak@gmail.com>
2019-07-29 21:47:46 -0300

    enemies!!!

--- player.gd
+++ player.gd
@@ -16,8 +16,20 @@ func _physics_process(delta):
 		if abs(move_vec.x) < (ACCEL*2):
 			move_vec.x = 0
 	
-	if Input.is_action_pressed("game_jump"):
-		move_vec.y = -3
-	if move_vec.y < MAX_GRAV:
-		move_vec.y += GRAV
+#	if Input.is_action_pressed("game_jump"):
+#		move_vec.y = -3
+#	if move_vec.y < MAX_GRAV:
+#		move_vec.y += GRAV
 	move_and_collide(move_vec)
+
+onready var sprite = $Sprite
+onready var hitbox = $Area2D
+var scary: bool
+func _process(_delta):
+	if Input.is_action_just_pressed("ui_accept"):
+		for node in hitbox.get_overlapping_bodies():
+			if node.is_in_group("enemy"):
+				node.scare()
+		sprite.frame = 1
+		yield(get_tree().create_timer(0.6), "timeout")
+		sprite.frame = 0
